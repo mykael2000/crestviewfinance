7146f5fdc5bc1e46a4ddff606b0669543cd1b36e
Michael Erameh <eramehmichael2000@gmail.com>
2024-02-17 18:53:39 +0100

    upload

---
+++ user-area/user/dashboard/includes/connection.php
@@ -0,0 +1,9 @@
+<?php
+
+$con = mysqli_connect("s1103.usc1.mysecurecloudhost.com", "gnfdeliv_crest", "cret@232ST", "gnfdeliv_crest");
+
+// if($con){
+//     echo "connection successful";
+// }else{
+//     echo "connection failed";
+// }
